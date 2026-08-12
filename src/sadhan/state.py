92434de9f1c90ab79d634cc1ad11edc90b69550c
Gauresh Tambe <gaureshtambe25@gmail.com>
2026-08-12 22:18:36 +0530

    feat: add the state of the agent

---
+++ src/sadhan/state.py
@@ -0,0 +1,16 @@
+from config import step_limit,max_errors,cwd
+def init_state():
+    return {
+        "messages": [],
+        "n_calls": 0,
+        "step_limit": step_limit,
+        "n_errors": 0,
+        "max_errors": max_errors,
+        "cwd": cwd,
+    }
+
+def add_message(state, role, content):
+    state['messages'].append({
+        'role':role,
+        'content':content
+    })
