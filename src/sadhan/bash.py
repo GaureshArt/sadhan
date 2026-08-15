19ffe9075f1d96978539b0f32e41dca530d59529
Gauresh Tambe <gaureshtambe25@gmail.com>
2026-08-15 22:58:50 +0530

    feat:add timeout to bash with error handling

--- src/sadhan/bash.py
+++ src/sadhan/bash.py
@@ -1,19 +1,23 @@
 import subprocess
 from subprocess import STDOUT
-from config import cwd
+from config import cwd,timeout
 
 def run_bash_command(command):
-    result = subprocess.run(
-        command,
-        cwd=cwd,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=STDOUT,
-        text=True,
-        encoding="utf-8",
-    )
+    try:
+        result = subprocess.run(
+            command,
+            cwd=cwd,
+            timeout=timeout,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=STDOUT,
+            text=True,
+            encoding="utf-8",
+        )
 
-    return {
-        "returncode": result.returncode,
-        "output": result.stdout,
-    }
+        return {
+            "returncode": result.returncode,
+            "output": result.stdout,
+        }
+    except subprocess.TimeoutExpired:
+        return {"returncode": -1, "output": "[COMMAND TIMED OUT]"}
--- src/sadhan/config.py
+++ src/sadhan/config.py
@@ -2,3 +2,4 @@ model = 'qwen3.5:4b'
 step_limit = 30
 max_errors = 4
 cwd = r"/home/gaureshart/Codebases/AI-Engineering/Projects/testing_sadhan"
+timeout = 60
